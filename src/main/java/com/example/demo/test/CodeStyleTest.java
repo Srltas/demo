ea9f1fa5b3a482713387795d80f5166c563104d9
Srltas <kdm97912@gmail.com>
2024-09-01 21:47:07 +0900

    chore: fix test repository

---
+++ src/main/java/com/example/demo/test/CodeStyleTest.java
@@ -0,0 +1,5 @@
+package com.example.demo.test;
+
+public class CodeStyleTest {
+public static void main(String[] args) {CodeStyleTest codeStyleTest = new CodeStyleTest();codeStyleTest.method01(1, "A", "B", "C", "D", "E");}
+public void method01(int arg1, String arg2, String arg3, String arg4, String arg5, String arg6) {System.out.println("arg1 : " + arg1 + "arg2 : " + arg2 + "arg3 : " + arg3 + "arg4 : " + arg4 + "arg5 : " + arg5 + "arg6 : " + arg6);}}
--- src/main/java/com/example/demo/test/TestController.java
+++ src/main/java/com/example/demo/test/TestController.java
@@ -5,9 +5,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
-
-    @GetMapping("/test")
-    public String test() {
-        return "TEST";
-    }
+	@GetMapping("/test")
+	public String test() {
+		return "TEST";
+	}
 }
